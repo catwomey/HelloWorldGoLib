fcd523f10d043f4af5d1aa1be10fda9d4ff685fa
ctwomey1 <chester.twomey@gmail.com>
2015-08-05 22:32:55 -0500

    Initial commit

---
+++ app/src/main/java/com/codingvelocity/helloworldgolib/HelloActivity.java
@@ -0,0 +1,40 @@
+package com.codingvelocity.helloworldgolib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import go.random.Random;
+public class HelloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+        TextView view = (TextView) findViewById(R.id.random);
+        view.setText(String.valueOf(Random.SudoRandomNumer()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_hello, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
